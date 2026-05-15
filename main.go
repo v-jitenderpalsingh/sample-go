f9899306d0ac39a720a0b21d567331c74ca563d5
Jitender Pal Singh <v-jitenderpalsingh@github.com>
2026-05-15 23:00:23 +0530

    updated test

--- main.go
+++ main.go
@@ -2,12 +2,8 @@ package main
 
 import (
     "fmt"
-
-    "github.com/lib/pq"
 )
 
 func main() {
     fmt.Println("Hello, World!")
-    // Reference pq so it's not removed by go mod tidy
-    _ = pq.ErrNotSupported
 }
